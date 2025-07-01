2a037b9c7d241f61867fb60df8a371b73ee75502
Bunjiro Yamada <bunjiroyamada@Takihyos-MacBook-Pro.local>
2025-07-01 15:41:49 -0400

    added main function: loaded data, created RandomForest, created GridSearchCV, generated RMSE

--- train.py
+++ train.py
@@ -6,10 +6,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
+from math import sqrt
+
 
 def load_data():
-    train = pd.read_csv("../data/train.csv") 
-    test = pd.read_csv("../data/test.csv")
+    train = pd.read_csv("../HousePrices/data/houses_filtered.csv") 
+    test = pd.read_csv("../HousePrices/data/test.csv")
     return train, test
 
 def preprocessing(X):
@@ -22,15 +24,68 @@ def preprocessing(X):
     ])
     
     categorical_transformer = Pipeline([
-        ('imputer', SimpleImputer(strategy = 'most_frequent'))
-        ('OneHotEncoder', OneHotEncoder(handle_unknown='ignore'))
+        ('imputer', SimpleImputer(strategy = 'most_frequent')),
+        ('OneHotEncoder', OneHotEncoder(handle_unknown='ignore')),
     ])
 
     preprocessor = ColumnTransformer([
-        ('num', numeric_transformer, num_cols)
-        ('cat', categorical_transformer, cat_cols)
+        ('num', numeric_transformer, num_cols),
+        ('cat', categorical_transformer, cat_cols),
     ])
     return preprocessor
 
 def main():
+    train_df, test_df = load_data()
+    X = train_df.drop("SalePrice", axis=1)
+    y = train_df["SalePrice"]
     
+    preprocessor = preprocessing(X)
+    model = RandomForestRegressor(random_state=44)
+
+    pipe = Pipeline([
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+
+    param_grid = {
+        'model__n_estimators': [100, 200],
+        'model__max_depth': [None, 10],
+    }
+
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=0.2, random_state=44
+    )
+
+    grid_search = GridSearchCV(
+        pipe,
+        param_grid,
+        cv=3,
+        scoring='neg_root_mean_squared_error',
+        n_jobs=1
+    )
+
+    grid_search.fit(X_train, y_train)
+    print('Best Parameters:', grid_search.best_params_)
+
+    best_model = grid_search.best_estimator_
+    y_pred = best_model.predict(X_val)
+    mse = mean_squared_error(y_val, y_pred)
+    rmse = sqrt(mse)
+    r2 = r2_score(y_val, y_pred)
+    print(f'RMSE: {rmse:.3f}')
+    print(f'R2: {r2:.3f}')
+
+    best_model.fit(X, y)
+    test_preds = best_model.predict(test_df)
+    submission = pd.DataFrame({
+        'Id': test_df['Id'],
+        'SalePrice': test_preds
+    })
+    submission.to_csv('submission.csv', index=False)
+
+if __name__ == "__main__":
+    main()
+
+
+
